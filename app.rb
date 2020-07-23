d6be80e9e71804d9101c957df697798b1343b3ab
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-07-23 22:39:35 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require 'colorize'
 require 'pry'
 Bundler.require
 
-#require_relative 'lib/application'
+
 require_relative 'lib/board'
 require_relative 'lib/BoardCase'
 require_relative 'lib/player'
 require_relative 'lib/game'
-#require_relative 'lib/show'
+
 
 Game.new.game_launch
 
